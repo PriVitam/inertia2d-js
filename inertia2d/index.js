604ed1564b136937f178509df7ab2cb847dbd387
Ria Chu <chuyue10@gmail.com>
2017-10-12 22:42:43 -0700

    Set up initial dev environment. (#1)

---
+++ inertia2d/index.js
@@ -0,0 +1,9 @@
+export default class HelloWorld {
+  constructor() {
+    this.helloWorld = 'Hello world';
+  }
+
+  getHelloWorld() {
+    return this.helloWorld;
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Sample test', () => {
+  it('should pass', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
